331a1cce7029395897c423c1ce10747adba0dcab
misturfixit <japullins420@gmail.com>
2017-09-18 14:27:44 -0400

    allllmost got it workin right...diddn't put my begin stmt..and an errant end floating arouond

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ get '/' do
 erb :root
 end
 post '/records' do
-  session[:r] = session[:r]
-  session[:r].class
+  session[:data] = session[:data]
+  session[:data].class
   wbinfo = {
     host:ENV['RDS_HOST'],
     port:ENV['RDS_PORT'],
@@ -18,13 +18,17 @@ post '/records' do
     password:ENV['RDS_PASSWORD']
   }
   wb = PG::Connection.new(wbinfo)
-  wb.exec "DROP TABLE IF EXISTS pb"
-  wb.exec "CREATE TABLE pb (First KEY, Last KEY,
-   	Street KEY,
-    City Key, 
-    State KEY, 
-    ZIP KEY, 
-    Phone KEY)"
+
+  wb.exec "CREATE TABLE pb (
+  	id int primary key,
+  	 fname varchar(50),
+     lname varchar(50),
+     addr varchar(50),
+     city varchar(25),
+     state varchar(2),
+     zip varchar(5),
+     phone varchar(10))"
+
   wb.exec "INSERT into pb VALUES(session[:r])"
   redirect '/get'
 end
@@ -36,21 +40,29 @@ get '/get_nfo' do
     user:ENV['RDS_USERNAME'],
     password:ENV['RDS_PASSWORD']
   }
-  
   wb = PG::Connection.new(wbinfo)
   rs = wb.exec 'SELECT * FROM pb LIMIT 7'
   	list = []
-   rs.each do |row|
-    list = "%s %s %s" % [row['First'], row['Last'], row['Street'], row['City'], row['State'], row['Zip'], row['Phone']]
+  		rs.each do |row|
+    list = "%s %s %s" % 
+      [row['First'],
+      row['Last'], 
+      row['Street'], 
+      row['City'],
+      row['State'], 
+      row['Zip'],
+      row['Phone']]
+			end
 end
-rescue PG::Error => e
-	puts e.message
-
+	rescue PG::Error => e
+		puts e.message
+	end
 ensure 
 	rs.clear if rs
 	wb.close if wb
-end
-erb :, locals:{list:list}
+
+
+erb :return, locals:{list:list}
 end
 
 
