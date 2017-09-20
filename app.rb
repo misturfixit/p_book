d4c4541c99cd3c566053caba61511f91a7a101a6
misturfixit <japullins420@gmail.com>
2017-09-20 14:11:47 -0400

    almost got it all together...tryin to get muh datapassed and then put it back into a usable form

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require 'aws-sdk'
 require 'pg'
 load './local_env.rb'
 enable 'sessions'
-
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 get '/' do
 	erb :input
 end
-
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 post '/get_nfo' do
   data = params[:data]
 begin
-
   pbinfo = {
     host: ENV['RDS_HOST'],
     port: ENV['RDS_PORT'],
@@ -31,27 +31,31 @@ pb.exec ("CREATE TABLE public.pb (
           State varchar(2),
           Zip varchar(5),
           Phone varchar(10))")
-
-pb.exec ("INSERT INTO public.pb (
-					id,
-			    F_name, L_name, 
-			    Street, City,
-		   	  State, Zip, Phone)
-	  VALUES('#{data[0]}',
-   '#{data[1]}', '#{data[2]}', 
-   '#{data[3]}', '#{data[4]}', 
-   '#{data[5]}', '#{data[6]}')");
-	  
-
-	rescue PG::Error => e
-    puts e.message
-	ensure
-    pb.close if pb
+					#++++++++++++++++#
+	pb.exec ("INSERT INTO public.pb (
+						id,
+				    F_name, L_name, 
+				    Street, City,
+			   	  State, Zip, Phone)
+		  VALUES('#{data[0]}',
+	   '#{data[1]}', '#{data[2]}', 
+	   '#{data[3]}', '#{data[4]}', 
+	   '#{data[5]}', '#{data[6]}')");
+		  
+rescue PG::Error => e
+	    puts e.message
+ensure
+	    pb.close if pb
+	end
+	data = data.to_s
+redirect '/return?data='+data
 end
-  redirect '/get'
-end
-
-get '/get' do
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+get '/return' do
+	data = params[:data]
+	data = data.join(",").to_a
+	list = params[:list]
+p "#{data}dadadadadadadadadadadadadda"
 	begin
 	  pbinfo = {
 	    host: ENV['RDS_HOST'],
@@ -62,28 +66,32 @@ get '/get' do
 	  }
 
 	  pb = PG::Connection.new(pbinfo)
-	  rs = pb.exec "SELECT * FROM pb LIMIT 7"
+	  rs = pb.exec("SELECT * FROM pb LIMIT 7") 
 
-		  list = []
-		  rs.each do |row|
-		    list << [row['First'], 
-				    row ['Last'], row['Street'],
-				    row ['City'], row['State'], 
-				    row ['Zip'], row['Phone']]
+	 vals = rs.values
+		 list = []
+		  vals.each do |row|
+		    list << row.values
+		     	list.each do |dat|
+     				dat 
+		     	end	
+		     #[row['First'], 
+				  #   row ['Last'], row['Street'],row ['City'], 
+				  #   row['State'], row ['Zip'], row['Phone']]
 		  end
 
-  #{}"%s %s %s %s %s %s %s" %
-
   rescue PG::Error => e
-
     puts e.message
-
   ensure
-    rs.clear if rs
     pb.close if pb
+erb :return, locals:{data:data,list:list}
 end
-# post '/' do
-#   erb :return, locals:{list: list}
-# end
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+post '/return' do
+	list = params[:list]
+erb :return, locals:{list: list}
+end
+
+
 end
 
