883f0abe823fd817e2407fce0632f2c61bea5774
misturfixit <japullins420@gmail.com>
2017-09-20 00:57:39 -0400

    something is wroooong...issues wit hthe sql

--- app.rb
+++ app.rb
@@ -1,68 +1,90 @@
-require "sinatra"
+require 'sinatra'
 require 'aws-sdk'
 require 'pg'
-require_relative "funk.rb"
 load './local_env.rb'
 enable 'sessions'
+
 get '/' do
-erb :root
+	erb :input
 end
-post '/records' do
-  session[:data] = session[:data]
-  session[:data].class
-  wbinfo = {
-    host:ENV['RDS_HOST'],
-    port:ENV['RDS_PORT'],
-    dbname:ENV['RDS_DATABASE'],
-    user:ENV['RDS_USERNAME'],
-    password:ENV['RDS_PASSWORD']
+
+post '/get_nfo' do
+  data = params[:data]
+begin
+
+  pbinfo = {
+    host: ENV['RDS_HOST'],
+    port: ENV['RDS_PORT'],
+    dbname: ENV['RDS_DB_NAME'],
+    user: ENV['RDS_USERNAME'],
+    password: ENV['RDS_PASSWORD']
   }
-  wb = PG::Connection.new(wbinfo)
 
-  wb.exec "CREATE TABLE pb (
-  	id int primary key,
-  	 fname varchar(50),
-     lname varchar(50),
-     addr varchar(50),
-     city varchar(25),
-     state varchar(2),
-     zip varchar(5),
-     phone varchar(10))"
+pb = PG::Connection.new(pbinfo)
 
-  wb.exec "INSERT into pb VALUES(session[:r])"
+pb.exec(create table "pb"(
+					id int primary key,
+          F_name varchar(50),
+          L_name varchar(50),
+          Street varchar(50)
+          City varchar(25),
+          Ztate varchar(2),
+          Zip varchar(5),
+          Phone varchar(10)
+          )
+
+pb.exec(INSERT INTO "pb"(id,
+			    F_name, L_name, 
+			    Street, City,
+		   	  State, Zip, Phone)
+	  VALUES('#{data[0]}',
+   '#{data[1]}', '#{data[2]}', 
+   '#{data[3]}', '#{data[4]}', 
+   '#{data[5]}', '#{data[6]}')
+	  )
+
+rescue PG::Error => e
+    puts e.message
+
+ensure
+    pb.close if pb
+end
   redirect '/get'
 end
-get '/get_nfo' do
-  wbinfo = {
-    host:ENV['RDS_HOST'],
+
+get '/get' do
+begin
+  pbinfo = {
+    host: ENV['RDS_HOST'],
     port:ENV['RDS_PORT'],
-    dbname:ENV['RDS_DATABASE'],
+    dbname:ENV['RDS_DB_NAME'],
     user:ENV['RDS_USERNAME'],
     password:ENV['RDS_PASSWORD']
   }
-  wb = PG::Connection.new(wbinfo)
-  rs = wb.exec 'SELECT * FROM pb LIMIT 7'
-  	list = []
-  		rs.each do |row|
-    list = "%s %s %s" % 
-      [row['First'],
-      row['Last'], 
-      row['Street'], 
-      row['City'],
-      row['State'], 
-      row['Zip'],
-      row['Phone']]
-			end
-end
-	rescue PG::Error => e
-		puts e.message
-	end
-ensure 
-	rs.clear if rs
-	wb.close if wb
 
+  pb = PG::Connection.new(pbinfo)
+  rs = pb.exec 'SELECT * FROM pb LIMIT 7'
 
-erb :return, locals:{list:list}
+	  list = []
+	  rs.each do |row|
+	    list << [row['First'], 
+	    row['Last'], row['Street'],
+	    row['City'], row['State'], 
+	    row['Zip'], row['Phone']]
+	  end
+
+  #{}"%s %s %s %s %s %s %s" %
+
+  rescue PG::Error => e
+
+    puts e.message
+
+  ensure
+    rs.clear if rs
+    pb.close if pb
 end
 
+  erb :input, locals:{list: list}
+
+end
 
