f5b22637e44df09ff243a74f08b05f9d23ef971f
misturfixit <japullins420@gmail.com>
2017-09-21 22:31:55 -0400

    Delete local_env.rb

--- local_env.rb
+++
@@ -1,6 +0,0 @@
-
-ENV['RDS_HOST'] = 'ta-dabase.cbjs4ttm8t0u.us-east-2.rds.amazonaws.com'
-ENV['RDS_PORT'] = '5432'
-ENV['RDS_DB_NAME'] = 'ta_dabase'
-ENV['RDS_USERNAME'] = 'Joe_P'
-ENV['RDS_PASSWORD'] = 'masterword'
